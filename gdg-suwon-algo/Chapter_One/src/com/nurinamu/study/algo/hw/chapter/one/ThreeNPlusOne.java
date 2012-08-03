510d95fc639815f1467bbb79e819c4a6e95f3611
nurinamu <nurinamu@gmail.com>
2012-08-03 14:05:58 +0900

    add trick.

--- gdg-suwon-algo/Chapter_One/src/com/nurinamu/study/algo/hw/chapter/one/ThreeNPlusOne.java
+++ gdg-suwon-algo/Chapter_One/src/com/nurinamu/study/algo/hw/chapter/one/ThreeNPlusOne.java
@@ -10,6 +10,8 @@ public class ThreeNPlusOne extends AlgoBase {
 	protected void algorithm() {
 		String[] inputArr = null;
 		while((inputArr = readNext()) != null){
+			countNumberWithTrick(Integer.parseInt(inputArr[0]), Integer.parseInt(inputArr[1]));
+			printTick();
 			countNumber(Integer.parseInt(inputArr[0]), Integer.parseInt(inputArr[1]));
 			printTick();
 		}
@@ -20,10 +22,39 @@ public class ThreeNPlusOne extends AlgoBase {
 		
 		for(int j = firstNum_p; j <= lastNum_p ; j++){
 			count = 1;
+			
 			for(int i=j;i!=1;i=(i%2==0)?(i/2):(3*i+1)){
 				//write(i+" ");
 				count++;
 				tick();
+			}
+//			write(count+"\n");
+			if(maxCount < count){
+				maxCount = count;
+			}
+		}
+		write(firstNum_p+" "+lastNum_p+" "+maxCount+"\n");
+	}
+	
+	private void countNumberWithTrick(int firstNum_p, int lastNum_p){
+		int count = 0, maxCount = 0, n;
+		
+		for(int j = firstNum_p; j <= lastNum_p ; j++){
+			count = 1;
+			
+			for(int i=j;i!=1;i=(i%2==0)?(i/2):(3*i+1)){
+				//write(i+" ");
+				tick();
+				
+				//trick code
+				n = (int)(Math.log(i)/Math.log(2));
+				if(Math.pow(2, n) == i){
+//					write("got it : "+i+"\n");
+					count += n;
+					break;
+				}else{
+					count++;
+				}
 			}
 //			write(count+"\n");
 			if(maxCount < count){
